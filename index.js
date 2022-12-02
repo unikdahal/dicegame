dbd10c6e08dae4ef21799a4d58d58a200104fb87
Ajay Khatri Chhetri <amajaying@gmail.com>
2022-12-03 03:15:38 +0530

    added roll dice button

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-var randomNumber1 = Math.random() * 6;
-randomNumber1 = Math.floor(randomNumber1) + 1;
+var btn = document.getElementById('new');
+var text = document.querySelectorAll('h1');
 
-var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+btn.addEventListener("click", function() {
+    var randomNumber1 = Math.random() * 6;
+    randomNumber1 = Math.floor(randomNumber1) + 1;
 
-document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
+    var randomDiceImage = "images/dice" + randomNumber1 + ".png";
 
-var randomNumber2 = Math.random() * 6;
-randomNumber2 = Math.floor(randomNumber2) + 1;
+    document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
 
-var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+    var randomNumber2 = Math.random() * 6;
+    randomNumber2 = Math.floor(randomNumber2) + 1;
 
-document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+    var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
 
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML("Player 1 won");
-} else if (randomNumber2 > randomNumber2) {
-  document.querySelector("h1").innerHTML("Player 2 Won");
-} else {
-  document.querySelector("h1").innerHTML("Draw");
-}
+    document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+
+    if (randomNumber1 > randomNumber2) {
+        text[0].innerHTML = "Player 1 Wins!";
+    } else if (randomNumber2 > randomNumber1) {
+        text[0].innerHTML = "Player 2 Wins!";
+    } else if (randomNumber1 === randomNumber2) {
+        text[0].innerHTML = "Draw!";
+    }
+});
